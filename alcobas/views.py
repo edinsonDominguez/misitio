8b3fd7f47e0669b4f7b55a2ad1685c5ca11b1038
edinson dominguez <edinsondom@gmail.com>
2024-12-19 16:31:42 -0500

    se ingresa una seccion para alcobas

--- alcobas/views.py
+++ alcobas/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def ver_alcoba(request):
+    return render(request, 'alcobas.html')
--- misitio/urls.py
+++ misitio/urls.py
@@ -15,17 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import primera_pagina, hora_actual, suma_hora, nombre_usuario, persona, persona_sistema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', primera_pagina, name = 'Home'),
+    path('pagina/', include('alcobas.urls')),
+    path('home', primera_pagina, name = 'Home'),
     path('hora', hora_actual, name = 'Hora'),
     path('suma_hora/<numero>', suma_hora, name = 'Suma'),
     path('nombre/<nombre>', nombre_usuario, name = 'Nombre'),
     path('persona', persona, name = 'Persona'),
     path('persona_sistema', persona_sistema, name = 'Persona2'),
     
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
