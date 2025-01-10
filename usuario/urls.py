5941244c4018ddb48492e32d76e79969c2b1a2c9
edinson dominguez <edinsondom@gmail.com>
2025-01-09 22:49:39 -0500

    se hizo la parte de registro y la parte de autenticacion del usuario

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from .views import Registro, login_usuario, cerrar_sesion
 
 urlpatterns = [
-    path('', views.registrar_usuario, name='Registro'),
-    path('login_user', views.login_usuario, name='Login')
+    path('', Registro.as_view(), name='Ingreso'),
+    path('cerrar_sesion', cerrar_sesion, name = 'cerrar_sesion'),
+    path('login_user', login_usuario, name='Login'),
 
 ]
