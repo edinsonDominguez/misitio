713989103424cf061711fdbe19058e4ed62d9360
edinson dominguez <edinsondom@gmail.com>
2025-01-14 17:07:42 -0500

    se configuro el admin, la tabla de producto

--- misitio/urls.py
+++ misitio/urls.py
@@ -22,7 +22,7 @@ from .views import primera_pagina, hora_actual, suma_hora, nombre_usuario, perso
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pagina/', include('alcobas.urls')),
+    path('pagina/', include('producto.urls')),
     path('usuario/', include('usuario.urls')),
     path('home', primera_pagina, name = 'Home'),
     path('hora', hora_actual, name = 'Hora'),
--- producto/models.py
+++ producto/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length='30')
+    nombre = models.CharField(max_length=30)
     valor = models.IntegerField()
     fecha_registro = models.DateField(auto_now_add=True)
     inventario = models.IntegerField()
     estado = models.BooleanField(auto_created=True)
+
+    def __str__(self):
+        return '%s %s' % (self.nombre, self.inventario)
