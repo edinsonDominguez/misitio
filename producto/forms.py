58113fdcb37bbb98ff844a436eaf7ff9b02179e5
edinson dominguez <edinsondom@gmail.com>
2025-01-18 10:12:13 -0500

    creando el formulario para el registro de productos en la vista usuario

--- producto/forms.py
+++ producto/forms.py
@@ -5,4 +5,4 @@ class Producto(forms.Form):
     nombre = forms.CharField(label='nombre', max_length=100)
     valor = forms.IntegerField(label='precio')
     inventario = forms.IntegerField(label='cantidad')
-    foto = forms.ImageField(label='imagen')
+    
