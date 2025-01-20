7e5356fd302e50196a8452a8b684cc07259f96db
edinson dominguez <edinsondom@gmail.com>
2025-01-20 10:36:49 -0500

    se actualizo el modelo de producto

--- producto/admin.py
+++ producto/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 
-from producto.models import Producto
+from producto.models import Producto,Categoria,Imagen
 
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('fecha_registro', 'nombre', 'valor', 'inventario')
+    list_display = ('fecha_registro', 'nombre', 'valor', 'inventario', 'categoria_producto', 'imagen_producto')
     list_filter = ('fecha_registro', 'inventario')
     ordering = ('valor',)
     search_fields = ('nombre',)
 
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+class ImagenAdmin(admin.ModelAdmin):
+    list_display = ('foto1', 'foto2', 'foto3', 'foto4', 'foto5', 'foto6',)
+
 
 admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Imagen, ImagenAdmin)
--- producto/models.py
+++ producto/models.py
@@ -1,12 +1,33 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return '%s' % (self.nombre)
+
+class Imagen(models.Model):
+    foto1 = models.ImageField(upload_to='producto')
+    foto2 = models.ImageField(upload_to='producto')
+    foto3 = models.ImageField(upload_to='producto')
+    foto4 = models.ImageField(upload_to='producto')
+    foto5 = models.ImageField(upload_to='producto')
+    foto6 = models.ImageField(upload_to='producto')
+    
+    def __str__(self):
+        return '%s' % (self.foto1)
+
 class Producto(models.Model):
     nombre = models.CharField(max_length=30)
     valor = models.IntegerField()
     fecha_registro = models.DateField(auto_now_add=True)
     inventario = models.IntegerField()
-    estado = models.BooleanField(auto_created=True)
-    foto_primaria = models.ImageField(upload_to='producto')
+    estado = models.BooleanField(default=True)
+    categoria_producto = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+    imagen_producto = models.ForeignKey(Imagen, on_delete = models.CASCADE)
+    
 
     def __str__(self):
         return '%s %s %s' % (self.nombre, self.valor, self.fecha_registro)
+    
+    
