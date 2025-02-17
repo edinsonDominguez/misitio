cb4ec90f5c6191106ebe702b7fe4ceb6487f4413
edinson dominguez <edinsondom@gmail.com>
2025-02-17 09:09:35 -0500

    se instalo crispy-forms para los formularios y se configuro

--- misitio/settings.py
+++ misitio/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'producto',
     'usuario',
     'home',
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
